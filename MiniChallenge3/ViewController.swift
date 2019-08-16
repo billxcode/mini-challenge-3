8831a39899977778c5c6efe56f56fd1f690b170e
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-08-16 16:20:22 +0700

    Initial Commit

---
+++ MiniChallenge3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniChallenge3
+//
+//  Created by Bill Tanthowi Jauhari on 16/08/19.
+//  Copyright © 2019 Batavia Hack Town. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

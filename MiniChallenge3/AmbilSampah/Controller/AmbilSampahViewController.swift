fd1f32ca564cbbeb78e60eb8257bd01f81530512
nandamocha <nanda.mocha@gmail.com>
2019-08-20 16:10:42 +0700

    no edit

--- MiniChallenge3/AmbilSampah/Controller/AmbilSampahViewController.swift
+++ MiniChallenge3/AmbilSampah/Controller/AmbilSampahViewController.swift
@@ -17,7 +17,6 @@ class AmbilSampahViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         
 //        configureLocation()
     }

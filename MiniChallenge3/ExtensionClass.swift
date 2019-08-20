b7ab433d4faba401f6ca2d09ccb800b93b85a413
nandamocha <nanda.mocha@gmail.com>
2019-08-20 21:23:34 +0700

    Set Basis, MVC + Extension class

---
+++ MiniChallenge3/ExtensionClass.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionClass.swift
+//  MiniChallenge3
+//
+//  Created by Nanda Mochammad on 20/08/19.
+//  Copyright © 2019 Batavia Hack Town. All rights reserved.
+//
+
+import Foundation
